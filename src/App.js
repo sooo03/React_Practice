1344121eaaa82646afdf054fad6e59289f29c288
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-25 15:36:40 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header className="App-header">
+        <input type="text" />
+        <button>추가</button>
+      </header>
+    </main>
+  );
+}
+
+export default App;
